e60ce73f1037bfffd8b1c6e5211bfbe5c563100c
vishwas raj batham51@gmail.com <vishwasrajbatham.151@gmail.com>
2024-12-08 21:50:19 +0530

    Fixing auto build config for workflow actions

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,17 @@ const Contact = () => {
       <div className='contact-header'>
         <h2>Contact Us</h2>
       </div>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdym4C-LP2F6Q7jvyVHXHStd22iW9JIVYTgW0hVy27xnIaa4g/viewform?embedded=true" width="640" height="915" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdym4C-LP2F6Q7jvyVHXHStd22iW9JIVYTgW0hVy27xnIaa4g/viewform?embedded=true" 
+        width="640" 
+        height="915" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0"
+        title="Descriptive title for the iframe"
+      >
+        Loading…
+      </iframe>
     </section>
   );
 };
